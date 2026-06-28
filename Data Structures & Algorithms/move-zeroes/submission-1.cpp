3424f49072b7c0e0b7f695527e546ae6651ac84b
Dhairya2306 <100343010+Dhairya2306@users.noreply.github.com>
2026-06-28 21:43:19 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        while(i<nums.size() && j<nums.size())
+        {
+            if(nums[i]==0 && nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+                i++;
+            }else if(nums[i]==0 && nums[j]==0)
+            {
+                j++;
+            }else
+            {
+                i++;
+                j++;
+            }
+        }
+        
+
+    }
+};
